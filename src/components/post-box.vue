1d9119568fdb5e6e9ab6f47e949614ff16b0db0a
lzh <13466083337@163.com>
2019-04-09 15:06:42 +0800

    do some job about index

---
+++ src/components/post-box.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="post-box">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: [],
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>首页</div>
+  <div class="post-index">
+    <div class="top-list">
+      <div class="top-post" v-for="(item, index) in topList" :key="index">
+        <span class="icon">置顶</span>
+        <span class="title">{{item.title}}</span>
+      </div>
+    </div>
+    <div class="margin-line"></div>
+    <div class="post-list">帖子专区</div>
+  </div>
 </template>
 
 <script>
@@ -12,7 +21,9 @@ export default {
 
   data () {
     return {
-      
+      topList: [],
+      postList: [],
+      page: 1
     }
   },
 
@@ -21,15 +32,66 @@ export default {
   },
 
   methods: {
-    
+    async getTopList(){
+      const res = await api.getTopList();
+      if(res.code){
+        tip.toast(res.code.message);
+        return;
+      }
+      this.topList = res.data.list;
+      console.log(this.topList);
+    }
   },
 
   created () {
     // let app = getApp()
+  },
+
+  onShow(){
+    this.getTopList();
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+.post-index {
+  background-color: #f5f5f5;
+  min-height: 100vh;
+}
+.top-list{
+  background-color: #ffffff;
+  width: 100%;
+  font-size: 16px;
+  margin-bottom: 10xp;
+  .top-post{
+    border-top: 1px solid #f5f5f5;
+    display: flex;
+    padding: 10px 15px;
+    .icon{
+      line-height: 20px;
+      text-align: center;
+      width: 60px;
+      color: #f5f5f5;
+      background-color: #4f2fe0;
+      margin-right: 10px;
+    }
+    .title{
+      line-height: 20px;
+      text-overflow: clip;
+    }
+  }
+  &:last-child{
+    border-bottom: 1px solid #f5f5f5;
+  }
+}
+.margin-line{
+  height: 10px;
+  width: 100%;
+  background-color: #f5f5f5;
+}
+.post-list{
+  background-color: #ffffff;
+  display: flex;
+  padding: 10px;
+}
 </style>
